5dd51bfe3c3f2ccb94ed284c63b6b4a73f45e7a4
AahilRafiq <aahil.rafiq136@gmail.com>
2024-08-12 16:07:06 +0530

    basic analyzer

---
+++ source.c
@@ -0,0 +1,20 @@
+/* Sum of Natural Numbers using for loop */
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    // loop starts here
+    for (i = 1; i <= n; ++i)
+    {
+        sum += i;
+    }
+
+    int _;
+    _ = 2;
+    
+
+    printf("Sum = %d", sum);
+    return 0;
+}
